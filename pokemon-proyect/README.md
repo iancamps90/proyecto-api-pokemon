7cbed955b905427319682ef342a587e4db570d37
iancamps90 <iancamps90@gmail.com>
2024-12-26 12:35:46 +0100

    actualización 2
    se añade el nombre, imagen y texto.
    ....se crea un identificador para se muestre si el pokemon existe o no con un terciario.

--- pokemon-proyect/README.md
+++ pokemon-proyect/README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+1. Se instala vite y react 
+2. se crea la estructura y se crea los ficheros pokemon- card y pokemon list
+3. se llama a la api de pokemon.
+4. se crea el set de pokemon card
+5. se añade el nombre, imagen y texto.
+6. se crea un identificador para se muestre si el pokemon existe o no con un terciario.
+7. 
--- pokemon-proyect/src/components/PokemonCard.jsx
+++ pokemon-proyect/src/components/PokemonCard.jsx
@@ -4,25 +4,29 @@ import { useEffect, useState } from "react"
 
 function PokemonCard() {
 
-    const [pokemon, setPokemon] = useState();
+    const [pokemon, setPokemon] = useState({});
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/1")
             .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => setPokemon(data));
         
         
     }, [])
 
 
 
-  return (
-      <li className="pokemon-card">
-        <h2 className="pokemon-name">{} </h2>
-
-          
-    </li>
-  )
+    return pokemon.id ? (
+        <li className="pokemon-card">
+            <h2 className="pokemon-name">{pokemon.name} </h2>
+            <img src={pokemon.sprites.front_default}
+                alt="pokemon img" className="pokemon-img" />
+            <h3 className="text">HP: {pokemon.stats[0].base_stat} </h3>
+        </li>
+    ) : (
+        <p className="loading">Loading...</p>
+    );
+    
 }
 
 export default PokemonCard
