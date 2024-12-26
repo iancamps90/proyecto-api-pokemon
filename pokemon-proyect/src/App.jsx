d3bf81b27ec17043ab8a19483fb2ff98032ce116
iancamps90 <iancamps90@gmail.com>
2024-12-26 13:23:13 +0100

    actualizacion 3
    se crea un css para el pokemon card y  añaden estilos generales

--- pokemon-proyect/src/App.jsx
+++ pokemon-proyect/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 import PokemonList from './components/PokemonList'
 
 function App() {
--- pokemon-proyect/src/components/PokemonCard.jsx
+++ pokemon-proyect/src/components/PokemonCard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-
+import "./PokemonCard.css"
 
 
 function PokemonCard() {
