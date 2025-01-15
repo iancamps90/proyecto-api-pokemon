e02b6e7d9912dadcf4ff82a453d9f3b023cf99db
iancamps90 <iancamps90@gmail.com>
2025-01-15 22:13:31 +0100

    compilar

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/proyecto-api-pokemon/',
   server: {
     hmr: true,
   },
