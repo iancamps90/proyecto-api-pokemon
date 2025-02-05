5d13f6bb0abf1912720b8226accad8e19b26c5d9
iancamps90 <iancamps90@gmail.com>
2025-02-05 06:55:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Esto asegura que la página principal sea http://localhost:5173/
+  base: '/proyecto-api-pokemon/', // Esto asegura que la página principal sea como http://localhost:5173/
   server: {
     hmr: true,
   },
